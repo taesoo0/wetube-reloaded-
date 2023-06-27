ddd35eaf1cceb5c8f03b5ef1bf76ea5d4a4a7349
taesoo0 <nm30207@gmail.com>
2023-06-28 07:50:20 +0900

    router

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # wetube-reloaded-
+
+/ -> Home
+/join -> Join
+/login -> Login
+/serach -> Search
+
+/users/edit -> Edit Profile
+/users/delete -> Delete Profile
+
+/videos/watch-> Watch Video
+/videos/edit-> Edit Video
+/videos/delete -> Delete Video
+/videos/comment -> Comment on a video
+/videos/comment/delete - > Delete A Commnet of a video
--- src/server.js
+++ src/server.js
@@ -1,21 +1,27 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const port = 4000;
 
 const app = express();
-
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
-
-const controller = (req, res) => {
-  return res.send("I love middleware");
-};
-
+const logger = morgan("dev");
 app.use(logger);
 
-app.get("/", controller);
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
+const home = (req, res) => {
+  console.log("I will response");
+  return res.send("hello");
+};
+
+const login = (req, res) => {
+  return res.send("login");
+};
 
 const handleListening = () => console.log(`Server listening on port ${port}`);
 
