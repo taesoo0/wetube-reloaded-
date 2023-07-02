4baeb9608fa585f7da774a81e0141018ef57e7a8
taesoo0 <nm30207@gmail.com>
2023-07-03 00:59:54 +0900

    fift-home

--- src/server.js
+++ src/server.js
@@ -8,10 +8,11 @@ const port = 4000;
 
 const app = express();
 const logger = morgan("dev");
-app.use(logger);
 
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
+app.use(logger);
+app.use(express.urlencoded({ extended: true }));
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
