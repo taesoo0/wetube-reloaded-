9e677d2d7cad964bd4cc1091c14297833bec03f3
taesoo0 <nm30207@gmail.com>
2023-07-06 09:35:41 +0900

    seventh-com

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,19 +1,61 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 export const getjoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postjoin = async (req, res) => {
-  const { name, username, email, password, location } = req.body;
-  await User.create({
-    name,
-    username,
-    email,
-    password,
-    location,
-  });
-  return res.redirect("/login");
+  const { name, username, email, password, password2, location } = req.body;
+  console.log(password, password2);
+  if (password !== password2) {
+    return res.status(400).render("join", {
+      pageTitle: "Join",
+      errorMessage: "password confirmation does not match.",
+    });
+  }
+  const exists = await User.exists({ $or: [{ username }, { email }] });
+  if (exists) {
+    return res.status(400).render("join", {
+      pageTitle: "Join",
+      errorMessage: "This username/email is already taken.",
+    });
+  }
+  try {
+    await User.create({
+      name,
+      username,
+      email,
+      password,
+      location,
+    });
+    return res.redirect("/login");
+  } catch (error) {
+    return res
+      .status(400)
+      .render("join", { pageTitle: Join, errorMessage: error._message });
+  }
+};
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
+
+export const postLogin = async (req, res) => {
+  const { username, password } = req.body;
+  const pageTitle = "Login";
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.status(400).render("login", {
+      pageTitle: pageTitle,
+      errorMessage: " AN account with this username dose not exist",
+    });
+  }
+  const ok = await bcrypt.compare(password, user.password);
+  if (!ok) {
+    return res.status(400).render("login", {
+      pageTitle: pageTitle,
+      errorMessage: "Wrong password",
+    });
+  }
+  return res.redirect("/");
 };
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
-export const login = (req, res) => res.send("Login");
 export const logout = (req, res) => res.send("Log out");
 export const see = (req, res) => res.send("See User");
